95d2b1d74fe947f1a351d190582854dc0e58cb12
Gauke Teijema <gauke@SchoolMaster.nl>
2020-12-07 12:01:13 +0100

    day 4 junit test.

--- src/test/java/gauket/mission2020/Day4Test.java
+++ src/test/java/gauket/mission2020/Day4Test.java
@@ -1,10 +1,10 @@
 package gauket.mission2020;
 
-import nl.gauket.mission2019.Day3;
 import nl.gauket.mission2020.Day4;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,7 +12,7 @@ public class Day4Test {
 
     @Test
     void part1() {
-        var input = new String[] {
+        var input = List.of(
             "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd",
             "byr:1937 iyr:2017 cid:147 hgt:183cm",
             "",
@@ -26,14 +26,55 @@ public class Day4Test {
             "",
             "hcl:#cfa07d eyr:2025 pid:166559648",
             "iyr:2011 ecl:brn hgt:59in"
-        };
+        );
         int expected = 2;
 
-        var result = Day4.solve(Arrays.asList(input), 1);
+        var result = Day4.solve(new ArrayList<>(input), 1);
         assertEquals(expected, result);
     }
 
     @Test
-    void part2() {
+    void part2_invalid() {
+        var input = List.of(
+            "eyr:1972 cid:100",
+            "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+            "",
+            "iyr:2019",
+            "hcl:#602927 eyr:1967 hgt:170cm",
+            "ecl:grn pid:012533040 byr:1946",
+            "",
+            "hcl:dab227 iyr:2012",
+            "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+            "",
+            "hgt:59cm ecl:zzz",
+            "eyr:2038 hcl:74454a iyr:2023",
+            "pid:3556412378 byr:2007"
+        );
+        int expected = 0;
+
+        var result = Day4.solve(new ArrayList<>(input), 2);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void part2_valid() {
+        var input = List.of(
+            "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980",
+            "hcl:#623a2f",
+            "",
+            "eyr:2029 ecl:blu cid:129 byr:1989",
+            "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+            "",
+            "hcl:#888785",
+            "hgt:164cm byr:2001 iyr:2015 cid:88",
+            "pid:545766238 ecl:hzl",
+            "eyr:2022",
+            "",
+            "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
+        );
+        int expected = 4;
+
+        var result = Day4.solve(new ArrayList<>(input), 2);
+        assertEquals(expected, result);
     }
 }
