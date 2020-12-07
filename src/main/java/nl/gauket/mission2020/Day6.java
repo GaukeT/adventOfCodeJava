e950bfd0d382b9c72167b9890b5a65162ad4e37d
Gauke Teijema <gauke@SchoolMaster.nl>
2020-12-07 16:17:50 +0100

    start day 6

--- src/main/java/nl/gauket/mission2020/Day6.java
+++ src/main/java/nl/gauket/mission2020/Day6.java
@@ -1,7 +1,11 @@
+package nl.gauket.mission2020;
+
+import nl.gauket.common.Day;
+
 import static nl.gauket.common.ResultLogger.printResult;
 
-public class Day1 extends Day {
-    private static final int DAY = 1;
+public class Day6 extends Day {
+    private static final int DAY = 6;
 
     public static void main(String[] args) {
         prepareDaily(YEAR20, DAY);
