d2b8ad77b2c07bba56b87db9692f70f60522a29c
Gauke Teijema <gauke@SchoolMaster.nl>
2020-12-10 14:36:57 +0100

    day 4 p2 debug

--- src/main/java/nl/gauket/mission2020/Day4.java
+++ src/main/java/nl/gauket/mission2020/Day4.java
@@ -2,10 +2,10 @@ package nl.gauket.mission2020;
 
 import nl.gauket.common.Day;
 import nl.gauket.common.InputReader;
+import nl.gauket.common.InputWriter;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -28,7 +28,6 @@ public class Day4 extends Day {
         start();
         // part 1 //
         var input = InputReader.readInputAsStringList(YEAR20, DAY);
-        input.add(""); // hack to validate last passport also
         var result = solve(input, 1);
 
         stop();
@@ -39,13 +38,15 @@ public class Day4 extends Day {
         start();
         // part 2 //
         var input = InputReader.readInputAsStringList(YEAR20, DAY);
-        var result = solve(input , 2);
+        var result = solve(input, 2);
 
         stop();
-        printResult(DAY, 186, result);
+        printResult(DAY, 186, result, false); // result is 1 higher than expected
     }
 
     public static int solve(List<String> input, int part) {
+        input.add(""); // hack to validate last passport also
+        var count = 0;
         var validCounter = 0;
         var ppBuilder = new StringBuilder();
 
@@ -58,6 +59,9 @@ public class Day4 extends Day {
                 var isValid = validatePassport(ppBuilder.toString(), part);
 
                 if (isValid) validCounter++;
+                if (part == 2) debugPass(ppBuilder.toString(), isValid);
+                count++;
+
                 ppBuilder = new StringBuilder();
             }
         }
@@ -148,5 +152,41 @@ public class Day4 extends Day {
             return value >= atLeast && value <= atMost;
         }
         return false;
+    }
+
+
+    // debug code
+    private static void debugPass(String pass, boolean isValid) {
+        var byr = getValue("byr:(\\d{4})", pass);
+        var iyr = getValue("iyr:(\\d{4})", pass);
+        var eyr = getValue("eyr:(\\d{4})", pass);
+        var hgt = getValue("hgt:((1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in)", pass);
+        var hcl = getValue("hcl:#[0-9|a-f]{6}", pass);
+        var ecl = getValue("ecl:(amb|blu|brn|gry|grn|hzl|oth)", pass);
+        var pid = getValue("pid:[0-9]{9}", pass);
+        var cid = getValue("cid:[0-9]{0,9}", pass);
+
+//        // --------------------------------------------------------
+//        if (byr.length() != 8) System.out.println("byr ERROR: " + pass);
+//        if (iyr.length() != 8) System.out.println("iyr ERROR: " + pass);
+//        if (eyr.length() != 8) System.out.println("eyr ERROR: " + pass);
+//        // --------------------------------------------------------
+//        if (hgt.length() != 9 && hgt.length() != 8) System.out.println("hgt ERROR: " + pass);
+//        // --------------------------------------------------------
+//        if (hcl.length() != 11) System.out.println("hcl ERROR: " + pass);
+//        if (ecl.length() != 7) System.out.println("ecl ERROR: " + pass);
+//        if (pid.length() != 13) System.out.println("pid ERROR: " + pass);
+//        // --------------------------------------------------------
+
+        InputWriter.debug(format("%s %s %s %s %s %s %s %s Valid:%s", iyr, byr, eyr, cid, hcl, ecl, hgt, pid, isValid));
+    }
+
+    private static String getValue(String regex, String pass) {
+        Pattern pattern = compile(regex);
+        Matcher matcher = pattern.matcher(pass);
+        if (matcher.find()) {
+            return matcher.group(0);
+        }
+        return "null";
     }
 }
