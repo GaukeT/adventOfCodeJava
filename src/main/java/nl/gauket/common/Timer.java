bc3b8e954db6157d13281e77972209efbadfeeb0
Gauke Teijema <gauke@SchoolMaster.nl>
2020-12-30 18:05:42 +0100

    more precise timer

--- src/main/java/nl/gauket/common/Timer.java
+++ src/main/java/nl/gauket/common/Timer.java
@@ -1,19 +1,24 @@
 package nl.gauket.common;
 
+import static java.lang.System.currentTimeMillis;
+
 public class Timer {
     private static long start;
 
     public static void start() {
-        start = System.currentTimeMillis();
+        start = System.nanoTime();
     }
 
     public static void stop() {
-        System.out.println("(" + (System.currentTimeMillis() - start) + "ms)");
+        var diff = (double) (System.nanoTime() - start) / 1_000_000;
+        System.out.println("(" + diff + "ms)");
         start = 0L;
     }
 
     public static void stop(String customMessage) {
-        System.out.println("" + customMessage + " (" + (System.currentTimeMillis() - start) + "ms)");
+        var diff = (double) (System.nanoTime() - start) / 1_000_000;
+        System.out.println("" + customMessage + " (" + diff + "ms)");
         start = 0L;
     }
 }
+
