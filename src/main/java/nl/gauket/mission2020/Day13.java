6dc891efedd51fdfae7eec5260a87b0e98c453db
Gauke Teijema <gauke@SchoolMaster.nl>
2021-01-06 13:07:29 +0100

    Day 13 p1+2

--- src/main/java/nl/gauket/mission2020/Day13.java
+++ src/main/java/nl/gauket/mission2020/Day13.java
@@ -2,17 +2,89 @@ package nl.gauket.mission2020;
 
 import nl.gauket.common.MyDay;
 
+import java.util.LinkedHashMap;
+
 public class Day13 extends MyDay {
 
     @Override
     public long[] solvePart1() {
-        var result = 0L;
-        return new long[]{result, 0L};
+        var result = solve(INPUT);
+        return new long[]{result, 207L};
     }
 
     @Override
     public long[] solvePart2() {
-        var result = 0L;
-        return new long[]{result, 0L};
+        var result = solve2(INPUT);
+        return new long[]{result, 530015546283687L};
+    }
+
+    static long solve(String[] input) {
+        var currTime = Integer.parseInt(input[0]);
+        var busses = input[1].replace(",x", "").split(",");
+
+        var result = check(currTime, busses);
+        var waitTime = result[0] - currTime;
+        var busId = result[1];
+
+        return waitTime * busId;
+    }
+
+    private static int[] check(int currTime, String[] busses) {
+        var waitTime = Integer.MAX_VALUE;
+        var busId = 0;
+
+        for (var bus : busses) {
+            if ("x".equals(bus)) continue;
+            var iBus = Integer.parseInt(bus);
+            var remainder = currTime % iBus;
+
+            if (remainder == 0) {
+                waitTime = currTime;
+                busId = iBus;
+                break;
+            }
+        }
+
+        if (waitTime == Integer.MAX_VALUE) {
+            currTime++;
+            return check(currTime, busses);
+        }
+
+        return new int[]{waitTime, busId};
+    }
+
+    static long solve2(String[] input) {
+        var busses = input[1].split(",");
+        var first = Integer.parseInt(busses[0]);
+        return validate(busses, first);
+    }
+
+    private static long validate(String[] busses, long first) {
+        var map = determineMap(busses);
+        long jump = first;
+        long timestamp = 0L;
+
+        for (var curr : map.keySet()) {
+            var index = map.get(curr);
+
+            while ((timestamp + index) % curr != 0) {
+                timestamp += jump;
+            }
+
+            jump *= curr;
+        }
+
+        return timestamp;
+    }
+
+    private static LinkedHashMap<Integer, Integer> determineMap(String[] busses) {
+        var map = new LinkedHashMap<Integer, Integer>();
+
+        for (int i = 1; i < busses.length; i++) {
+            if ("x".equals(busses[i])) continue;
+            var curr = Integer.parseInt(busses[i]);
+            map.put(curr, i);
+        }
+        return map;
     }
 }
---
+++ src/test/java/nl/gauket/mission2020/Day13Test.java
@@ -0,0 +1,91 @@
+package nl.gauket.mission2020;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Day13Test {
+    @Test
+    void part1() {
+        var input = new String[]{
+                "939",
+                "7,13,x,x,59,x,31,19"
+        };
+        var expected = 295L;
+
+        var result = Day13.solve(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void part2() {
+        var input = new String[]{
+                "ignore",
+                "7,13,x,x,59,x,31,19"
+        };
+        var expected = 1068781L;
+
+        var result = Day13.solve2(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void part2b() {
+        var input = new String[]{
+                "ignore",
+                "17,x,13,19"
+        };
+        var expected = 3417L;
+
+        var result = Day13.solve2(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void part2c() {
+        var input = new String[]{
+                "ignore",
+                "67,7,59,61"
+        };
+        var expected = 754018L;
+
+        var result = Day13.solve2(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void part2d() {
+        var input = new String[]{
+                "ignore",
+                "67,x,7,59,61"
+        };
+        var expected = 779210L;
+
+        var result = Day13.solve2(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void part2e() {
+        var input = new String[]{
+                "ignore",
+                "67,7,x,59,61"
+        };
+        var expected = 1261476L;
+
+        var result = Day13.solve2(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void part2f() {
+        var input = new String[]{
+                "ignore",
+                "1789,37,47,1889"
+        };
+        var expected = 1202161486L;
+
+        var result = Day13.solve2(input);
+        assertEquals(expected, result);
+    }
+}
