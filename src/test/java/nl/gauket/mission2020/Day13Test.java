e1c6893c999ac79986e830211e604bf5e78da77d
Gauke Teijema <gauke@SchoolMaster.nl>
2021-01-07 15:12:17 +0100

    optimized Junit tests

--- src/test/java/nl/gauket/mission2020/Day13Test.java
+++ src/test/java/nl/gauket/mission2020/Day13Test.java
@@ -1,6 +1,11 @@
 package nl.gauket.mission2020;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,75 +22,21 @@ class Day13Test {
         assertEquals(expected, result);
     }
 
-    @Test
-    void part2() {
-        var input = new String[]{
-                "ignore",
-                "7,13,x,x,59,x,31,19"
-        };
-        var expected = 1068781L;
-
+    @ParameterizedTest(name = "{index}, input: {0}")
+    @MethodSource
+    void part2(String[] input, long expected) {
         var result = Day13.solve2(input);
         assertEquals(expected, result);
     }
 
-    @Test
-    void part2b() {
-        var input = new String[]{
-                "ignore",
-                "17,x,13,19"
-        };
-        var expected = 3417L;
-
-        var result = Day13.solve2(input);
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void part2c() {
-        var input = new String[]{
-                "ignore",
-                "67,7,59,61"
-        };
-        var expected = 754018L;
-
-        var result = Day13.solve2(input);
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void part2d() {
-        var input = new String[]{
-                "ignore",
-                "67,x,7,59,61"
-        };
-        var expected = 779210L;
-
-        var result = Day13.solve2(input);
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void part2e() {
-        var input = new String[]{
-                "ignore",
-                "67,7,x,59,61"
-        };
-        var expected = 1261476L;
-
-        var result = Day13.solve2(input);
-        assertEquals(expected, result);
-    }
-
-    @Test
-    void part2f() {
-        var input = new String[]{
-                "ignore",
-                "1789,37,47,1889"
-        };
-        var expected = 1202161486L;
-
-        var result = Day13.solve2(input);
-        assertEquals(expected, result);
+    private static Stream<Arguments> part2() {
+        return Stream.of(
+                Arguments.of(new String[]{"ignore","7,13,x,x,59,x,31,19"}, 1068781L),
+                Arguments.of(new String[]{"ignore","17,x,13,19"}, 3417L),
+                Arguments.of(new String[]{"ignore","67,7,59,61"}, 754018L),
+                Arguments.of(new String[]{"ignore","67,x,7,59,61"}, 779210L),
+                Arguments.of(new String[]{"ignore","67,7,x,59,61"}, 1261476L),
+                Arguments.of(new String[]{"ignore","1789,37,47,1889"}, 1202161486L)
+        );
     }
 }
