7a9a9042c5bd3dc27290dc6d6b6e8f6189f680b5
GaukeT <gauket.dev@gmail.com>
2021-12-08 19:59:07 +0100

    Year 2021, Day 6, part 2

--- src/main/java/nl/gauket/mission2021/Day6.java
+++ src/main/java/nl/gauket/mission2021/Day6.java
@@ -3,7 +3,6 @@ package nl.gauket.mission2021;
 import nl.gauket.common.InputReader;
 import nl.gauket.common.NewDay;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Day6 extends NewDay {
@@ -21,11 +20,11 @@ public class Day6 extends NewDay {
     @Override
     public long[] solvePart2() {
         var result = solve(INPUT_INT, 256);
-        return new long[]{result}; // 686400344 too low
+        return new long[]{result, 1644286074024L};
     }
 
     public static long solve(int[] input, int days) {
-        var result = new int[9];
+        var result = new long[9];
 
         for (var fish : input) {
             result[fish]++;
@@ -42,41 +41,6 @@ public class Day6 extends NewDay {
             result[6] += count; // evolved fishes reset timer to 6
         }
 
-        return Arrays.stream(result).reduce(0, Integer::sum);
-    }
-
-    // out of memory exception in part 2
-    public static long solveArr(int[] input, int days) {
-        for (int i = 0; i < days; i++) {
-            var grow = new ArrayList<Integer>();
-
-            for (int j = input.length - 1; j >= 0; j--) {
-                var temp = input[j];
-                temp--;
-
-                if (temp < 0) {
-                    temp = 6;
-                    grow.add(8);
-                }
-
-                input[j] = temp;
-            }
-
-            if (!grow.isEmpty()) {
-                var tempArr = new int[input.length + grow.size()];
-                for (int j = 0; j < tempArr.length; j++) {
-                    if (j < input.length) {
-                        tempArr[j] = input[j];
-                    } else {
-                        tempArr[j] = grow.get(j - input.length);
-                    }
-                }
-                input = tempArr;
-            }
-
-            print("day: " + (i + 1) + " total: " + input.length);
-        }
-
-        return input.length;
+        return Arrays.stream(result).reduce(0, Long::sum);
     }
 }
--- src/test/java/nl/gauket/mission2021/Day6Test.java
+++ src/test/java/nl/gauket/mission2021/Day6Test.java
@@ -12,21 +12,6 @@ class Day6Test {
 
     @ParameterizedTest(name = "{index}, days: {1} - input: {0}")
     @MethodSource
-    void solveArr(int[] input, int days, long expected) {
-        var result = Day6.solveArr(input, days);
-        assertEquals(expected, result);
-    }
-
-    private static Stream<Arguments> solveArr() {
-        return Stream.of(
-                Arguments.of(new int[]{ 3,4,3,1,2 }, 18, 26),
-                Arguments.of(new int[]{ 3,4,3,1,2 }, 80, 5934)
-//                Arguments.of(new int[]{ 3,4,3,1,2 }, 256, 26984457539L)
-        );
-    }
-
-    @ParameterizedTest(name = "{index}, days: {1} - input: {0}")
-    @MethodSource
     void solve(int[] input, int days, long expected) {
         var result = Day6.solve(input, days);
         assertEquals(expected, result);
@@ -35,8 +20,8 @@ class Day6Test {
     private static Stream<Arguments> solve() {
         return Stream.of(
                 Arguments.of(new int[]{ 3,4,3,1,2 }, 18, 26),
-                Arguments.of(new int[]{ 3,4,3,1,2 }, 80, 5934)
-//                Arguments.of(new int[]{ 3,4,3,1,2 }, 256, 26984457539L)
+                Arguments.of(new int[]{ 3,4,3,1,2 }, 80, 5934),
+                Arguments.of(new int[]{ 3,4,3,1,2 }, 256, 26984457539L)
         );
     }
 }
