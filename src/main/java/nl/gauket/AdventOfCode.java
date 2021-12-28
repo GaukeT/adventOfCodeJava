e9d31b6a00bd79c804a35cafee1161f0c34d438c
GaukeT <gauket.dev@gmail.com>
2021-12-28 17:06:25 +0100

    Run all advent of code solutions

--- src/main/java/nl/gauket/AdventOfCode.java
+++ src/main/java/nl/gauket/AdventOfCode.java
@@ -1,18 +1,52 @@
 package nl.gauket;
 
-import nl.gauket.mission2019.Y2019;
-import nl.gauket.mission2020.Y2020;
-import nl.gauket.mission2021.Y2021;
+import nl.gauket.common.NewDay;
+
+import java.lang.reflect.InvocationTargetException;
+import java.time.LocalDate;
+import java.time.Month;
 
 public class AdventOfCode {
     public static void main(String[] args) {
-        System.out.println("\n> Year2019: ");
-        Y2019.main(args);
+        // runSpecificYearAndDay(2021, 1);
+        // runYear(2021);
+        runAll();
+    }
 
-        System.out.println("\n> Year2020: ");
-        Y2020.main(args);
+    private static void runSpecificYearAndDay(int year, int day) {
+        var obj = getDayInstanceOf(year, day);
+        if (obj != null) {
+            obj.run(year, false);
+        }
+    }
 
-        System.out.println("\n> Year2021:");
-        Y2021.main(args);
+    private static void runYear(int year) {
+        System.out.println("\n> Task :Year" + year);
+        for (int day = 1; day <= 25; day++) {
+            runSpecificYearAndDay(year, day);
+        }
+    }
+
+    private static void runAll() {
+        for (int year = 2015; year <= determineLatestAdventOfCode(); year++) {
+            runYear(year);
+        }
+    }
+
+    private static int determineLatestAdventOfCode() {
+        var curr = LocalDate.now();
+        return curr.getMonth().equals(Month.DECEMBER) ? curr.getYear() : curr.getYear() - 1;
+    }
+
+    private static NewDay getDayInstanceOf(int year, int day) {
+        try {
+            return (NewDay) Class.forName("nl.gauket.mission" + year + ".Day" + day).getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            // e.printStackTrace();
+        }
+
+        return null;
     }
 }
