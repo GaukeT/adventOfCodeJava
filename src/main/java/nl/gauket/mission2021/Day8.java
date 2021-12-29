463d2f108f0cb63416ce87ceeb5b280ad6aaa5fb
GaukeT <gauket.dev@gmail.com>
2021-12-29 16:14:04 +0100

    Year 2021, Day 8

---
+++ src/main/java/nl/gauket/mission2021/Day8.java
@@ -0,0 +1,15 @@
+package nl.gauket.mission2021;
+
+import nl.gauket.common.NewDay;
+
+public class Day8 extends NewDay {
+    @Override
+    public long[] solvePart1() {
+        return new long[0];
+    }
+
+    @Override
+    public long[] solvePart2() {
+        return new long[0];
+    }
+}
